12d4a8818489bafee8eea46e865d4469f12f5072
ravenkong28 <127236224+ravenkong28@users.noreply.github.com>
2023-03-12 18:44:27 +0700

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import AboutImg from '../public/assets/about.jpg';
+import AboutImg from '../public/assets/about.jpeg';
 
 const About = () => {
   return (
@@ -13,23 +13,19 @@ const About = () => {
           </p>
           <h2 className='py-4'>Who I Am</h2>
           <p className='py-2 text-gray-600'>
-            I specialize in building mobile responsive front-end UI applications
-            that connect with API’s and other backend technologies. I’m
-            passionate about learning new technologies and understand there is
-            more than one way to accomplish a task. Though I am most proficient
-            in building front-end applications using HTML, CSS, Javascript, and
-            React, I am a quick learner and can pick up new tech stacks as
-            needed. I believe that being a great developer is not using one
-            specific language, but choosing the best tool for the job.
+            I'm a college student with a great passion for analysis data, 
+            make and develop an applications, and all the related field. 
+            I’m passionate about learning new technologies and a hard 
+            worker and want to develop any kind of new skill as needed.
           </p>
-          <p className='py-2 text-gray-600'>
+          {/* <p className='py-2 text-gray-600'>
             I started web developement in 2013 managing multiple e-commerce
             websites on CMS platforms such as WordPress, BigCommerce, and
             Shopify. I have experience working directly with clients and taking
             mock wireframes all the way to deployed applications. In my spare
             time I run Code Commerce, a Youtube channel where I teach web
             developement and various front-end technologies.
-          </p>
+          </p> */}
           <Link href='/#projects'>
             <p className='py-2 text-gray-600 underline cursor-pointer'>
               Check out some of my latest projects.
