f98a48ef850c933b033240f6f0c2c4ea0274cab0
ravenkong28 <127236224+ravenkong28@users.noreply.github.com>
2023-03-12 19:06:07 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
 import Head from 'next/head'
-import About from '../components/About'
-import Contact from '../components/Contact'
 import Main from '../components/Main'
-import Projects from '../components/Projects'
-import Skills from '../components/Skills'
+import About from '../components/About';
+import Skills from '../components/Skills';
+import Projects from '../components/Projects';
+import Contact from '../components/Contact';
 
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Clint | Front-End Developer</title>
-        <meta name="description" content="I’m a front-end web developer specializing in building (and occasionally designing) exceptional digital experiences." />
+        <title>Raven | Portofolio </title>
+        <meta name="description" content="Portofolio"/>
         <link rel="icon" href="/fav.png" />
       </Head>
     <Main />
